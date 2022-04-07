853d67cb8e94ce1df435ad386c44523d456c9795
Gavin Sherman <gavinmsherman@gmail.com>
2022-04-07 13:22:43 -0400

    added name input to form

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Cupcake Form</title>
+</head>
+<body>
+<h1>Cupcake Fundraiser</h1>
+
+<form id="cupcake-form" action="#" method="post">
+    <label>Your Name:</label>
+<input type="text" id="name" placeholder="Enter your name!" name="name">
+</form>
+</body>
+</html>
