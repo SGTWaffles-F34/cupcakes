50909c31881eb458d1b9571e2cedee89a09beeb2
Gavin Sherman <gavinmsherman@gmail.com>
2022-04-07 13:34:11 -0400

    completed building the form

--- index.php
+++ index.php
@@ -13,6 +13,20 @@
 <form id="cupcake-form" action="#" method="post">
     <p>Your Name:</p>
 <input type="text" id="name" placeholder="Enter your name!" name="name">
+    <br>
+    <p>Cupcake Flavors</p>
+    <br>
+
+    <ul>
+        <li><input type="checkbox" name="flavor[]"> Chocolate</li>
+        <li><input type="checkbox" name="flavor[]"> Vanilla</li>
+        <li><input type="checkbox" name="flavor[]"> Tiramisu</li>
+        <li><input type="checkbox" name="flavor[]"> Whiskey Maple Bacon</li>
+        <li><input type="checkbox" name="flavor[]"> Red Velvet</li>
+        <li><input type="checkbox" name="flavor[]"> Carrot Cake</li>
+    </ul>
+
+    <button type="submit" id="submit">Submit</button>
 </form>
 </body>
 </html>
