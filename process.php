78d4827ac1161d243bd5886fb0fa85462fa345d6
Gavin Sherman <gavinmsherman@gmail.com>
2022-04-11 22:19:41 -0400

    added php side validation

--- process.php
+++ process.php
@@ -9,35 +9,47 @@ Description: cupcake confirmation
     return $flavor;
 }*/
 
+$isValid = true;
+
 $name = $_GET['name'];
 $flavor = implode("<li>", $_GET['flavor']);
 
-define("Cupcake_Cost", 3.50);
-
-$numCupcakes = sizeof($_GET['flavor']);
-
-$price = 0.0;
-$price += $numCupcakes * Cupcake_Cost;
-//$price += $flavor * Cupcake_Cost;
-
-$price = number_format($price, 2);
-
-
-thanks($name);
-echo '<h3>Order Summary:</h3>';
-echo"<p><li>$flavor</li></p>";
-echo"<p>Order Total: $price</p>";
-
-function thanks($name = "")
-{
-    $msg = "<h1> Thank you, $name, for your order!";
-    /*if($name != "")
-    {
-        $msg .= ", $name";
-    }
-    $msg .= "!</h1>";*/
-    echo $msg;
+//check if a name was given
+if($name.is_null()){
+    $isValid = false;
 }
 
+define("Cupcake_Cost", 3.50);
 
+if(!$isValid){
+    echo "Sorry you didn't provide a name for your order! try again please!";
+}
+else {
+
+    $numCupcakes = sizeof($_GET['flavor']);
+
+    $price = 0.0;
+    $price += $numCupcakes * Cupcake_Cost;
+//$price += $flavor * Cupcake_Cost;
+
+    $price = number_format($price, 2);
+
+
+    thanks($name);
+    echo '<h3>Order Summary:</h3>';
+    echo "<p><li>$flavor</li></p>";
+    echo "<p>Order Total: $price</p>";
+
+    function thanks($name = "")
+    {
+        $msg = "<h1> Thank you, $name, for your order!";
+        /*if($name != "")
+        {
+            $msg .= ", $name";
+        }
+        $msg .= "!</h1>";*/
+        echo $msg;
+    }
+
+}
 ?>
