c5b3ad852ec7836a251e92df7f050f42099afc76
Gavin Sherman <gavinmsherman@gmail.com>
2022-04-11 22:07:26 -0400

    Syncing with partners database

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<!--
+Angel Campos
+Gavin Sherman
+April 7, 2022
+https://acampos.greenriverdev.com/328sdev/team-programming/
+Description: cupcake request form
+-->
 <?php
 ?>
 
@@ -6,26 +13,43 @@
 <head>
     <meta charset="UTF-8">
     <title>Cupcake Form</title>
+    <link rel="stylesheet" href="style/cupcake.css">
+
 </head>
 <body>
-<h1>Cupcake Fundraiser</h1>
 
-<form id="cupcake-form" action="process.php" method="post">
-    <p>Your Name:</p>
-<input type="text" id="name" placeholder="Enter your name!" name="name">
+<h2>Cupcake Fundraiser</h2>
+
+<div class="main">
+<form id="cupcake-form" action="process.php" method="get">
+    <div class="form-group">
+        <p>Your Name:</p>
+        <input type="text" id="name" placeholder="Please enter your name" name="name">
+        <span class="err" id="err-name">Please Enter Name</span>
+    </div>
     <br>
-    <p>Cupcake Flavors</p>
+    <div class="form-group">
+        <p>Cupcake Flavors:</p>
 
-    <ul style="list-style-type: none">
-        <li><input type="checkbox" name="flavor[]" value="Chocolate"> Chocolate</li>
-        <li><input type="checkbox" name="flavor[]" value="Vanilla"> Vanilla</li>
-        <li><input type="checkbox" name="flavor[]" value="Tiramisu"> Tiramisu</li>
-        <li><input type="checkbox" name="flavor[]" value="Whiskey Maple Bacon"> Whiskey Maple Bacon</li>
-        <li><input type="checkbox" name="flavor[]" value="Red Velvet"> Red Velvet</li>
-        <li><input type="checkbox" name="flavor[]" value="Carrot Cake"> Carrot Cake</li>
-    </ul>
+        <ul style="list-style-type: none">
+            <li><input type="checkbox" name="flavor[]" value="Chocolate"> Chocolate</li>
+            <li><input type="checkbox" name="flavor[]" value="Vanilla"> Vanilla</li>
+            <li><input type="checkbox" name="flavor[]" value="Tiramisu"> Tiramisu</li>
+            <li><input type="checkbox" name="flavor[]" value="Whiskey Maple Bacon"> Whiskey Maple Bacon</li>
+            <li><input type="checkbox" name="flavor[]" value="Red Velvet"> Red Velvet</li>
+            <li><input type="checkbox" name="flavor[]" value="Carrot Cake"> Carrot Cake</li>
+        </ul>
+        <span class="err" id="err-flavor">Please select a cupcake flavor</span>
+    </div>
 
-    <button type="submit" id="submit">Submit</button>
+        <button type="submit" id="submit">Order</button>
 </form>
+</div>
+
+
+
+
+<?php
+?>
 </body>
 </html>
